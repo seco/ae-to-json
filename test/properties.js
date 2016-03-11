1471c5f0c06658450aa2d175ae155f22acc0aad2
Mikko Haapoja <me@mikkoh.com>
2016-03-11 17:57:38 -0500

    Put in expected values for properties

--- test/properties.js
+++ test/properties.js
@@ -101,7 +101,37 @@ function getExpected() {
             1200,
             1920,
             0
-          ]
+          ],
+          {
+            "in": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 0,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                0,
+                0
+              ]
+            },
+            "out": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 0,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                0,
+                0
+              ]
+            }
+          }
         ],
         [
           34.6012679346013,
@@ -109,7 +139,37 @@ function getExpected() {
             1200,
             1920,
             0
-          ]
+          ],
+          {
+            "in": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 0,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                0,
+                0
+              ]
+            },
+            "out": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 8.57435862995299,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                46.5107803344727,
+                0
+              ]
+            }
+          }
         ],
         [
           84.2842842842843,
@@ -117,7 +177,37 @@ function getExpected() {
             1200,
             2346,
             0
-          ]
+          ],
+          {
+            "in": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 8.57435862995299,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                -46.5107803344727,
+                0
+              ]
+            },
+            "out": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 0,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                0,
+                0
+              ]
+            }
+          }
         ]
       ],
       "scale": [
@@ -133,7 +223,51 @@ function getExpected() {
       "opacity": [
         [
           0,
-          100
+          100,
+          {
+            "in": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 0,
+                  "influence": 16.666666667
+                }
+              ]
+            },
+            "out": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": -1.03594884203249,
+                  "influence": 16.666666667
+                }
+              ]
+            }
+          }
+        ],
+        [
+          96.5298631965299,
+          0,
+          {
+            "in": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": -1.03594884203249,
+                  "influence": 16.666666667
+                }
+              ]
+            },
+            "out": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 0,
+                  "influence": 16.666666667
+                }
+              ]
+            }
+          }
         ]
       ],
       "rotationX": [
@@ -173,7 +307,37 @@ function getExpected() {
             1200,
             1920,
             0
-          ]
+          ],
+          {
+            "in": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 0,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                -71,
+                0
+              ]
+            },
+            "out": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 5.61690277166739,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                71,
+                0
+              ]
+            }
+          }
         ],
         [
           84.2842842842843,
@@ -181,7 +345,37 @@ function getExpected() {
             1200,
             2346,
             0
-          ]
+          ],
+          {
+            "in": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 5.61690277166739,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                -71,
+                0
+              ]
+            },
+            "out": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 0,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                71,
+                0
+              ]
+            }
+          }
         ]
       ],
       "scale": [
@@ -220,6 +414,262 @@ function getExpected() {
       ]
     },
     "Test-Comp-Animated_staticLogo_2": {
+      "anchorPoint": [
+        [
+          0,
+          [
+            250,
+            250,
+            0
+          ]
+        ]
+      ],
+      "position": [
+        [
+          0,
+          [
+            256,
+            1948,
+            0
+          ]
+        ]
+      ],
+      "scale": [
+        [
+          0,
+          [
+            100,
+            100,
+            100
+          ]
+        ]
+      ],
+      "opacity": [
+        [
+          0,
+          100
+        ]
+      ],
+      "rotationX": [
+        [
+          0,
+          0
+        ]
+      ],
+      "rotationY": [
+        [
+          0,
+          0
+        ]
+      ],
+      "rotationZ": [
+        [
+          0,
+          0
+        ]
+      ]
+    },
+    "Test-Comp-Animated_animatedLogoBezier_2": {
+      "anchorPoint": [
+        [
+          0,
+          [
+            250,
+            250,
+            0
+          ]
+        ]
+      ],
+      "position": [
+        [
+          8.44177510844177,
+          [
+            1200,
+            1920,
+            0
+          ],
+          {
+            "in": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 0,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                -71,
+                0
+              ]
+            },
+            "out": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 30.5525846882257,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                34.6377944946289,
+                0
+              ]
+            }
+          }
+        ],
+        [
+          45.2118785452119,
+          [
+            1704.17801874055,
+            1645.56064015018,
+            -954.177061094476
+          ],
+          {
+            "in": {
+              "type": "bezier",
+              "temporalEase": [
+                {
+                  "speed": 31.8341262537217,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                -71,
+                0
+              ]
+            },
+            "out": {
+              "type": "bezier",
+              "temporalEase": [
+                {
+                  "speed": 31.8341262537217,
+                  "influence": 26.3261116176607
+                }
+              ],
+              "spatialTangent": [
+                0,
+                3.4916205406189,
+                0
+              ]
+            }
+          }
+        ],
+        [
+          55.8558558558559,
+          [
+            1587.27796637104,
+            1806.74867938474,
+            222.754193566184
+          ],
+          {
+            "in": {
+              "type": "bezier",
+              "temporalEase": [
+                {
+                  "speed": 32.8049415267424,
+                  "influence": 38.7475352828698
+                }
+              ],
+              "spatialTangent": [
+                67.6812591552734,
+                -90.99951171875,
+                -128.089492797852
+              ]
+            },
+            "out": {
+              "type": "bezier",
+              "temporalEase": [
+                {
+                  "speed": 32.8049415267424,
+                  "influence": 36.3243926412931
+                }
+              ],
+              "spatialTangent": [
+                -148.936920166016,
+                200.250228881836,
+                281.869079589844
+              ]
+            }
+          }
+        ],
+        [
+          84.2842842842843,
+          [
+            1200,
+            2346,
+            0
+          ],
+          {
+            "in": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 27.9231623368901,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                -21.0659446716309,
+                0
+              ]
+            },
+            "out": {
+              "type": "linear",
+              "temporalEase": [
+                {
+                  "speed": 0,
+                  "influence": 16.666666667
+                }
+              ],
+              "spatialTangent": [
+                0,
+                71,
+                0
+              ]
+            }
+          }
+        ]
+      ],
+      "scale": [
+        [
+          0,
+          [
+            100,
+            100,
+            100
+          ]
+        ]
+      ],
+      "opacity": [
+        [
+          0,
+          100
+        ]
+      ],
+      "rotationX": [
+        [
+          0,
+          34
+        ]
+      ],
+      "rotationY": [
+        [
+          0,
+          46
+        ]
+      ],
+      "rotationZ": [
+        [
+          0,
+          -4
+        ]
+      ]
+    },
+    "Test-Comp-Animated_staticLogo_3": {
       "anchorPoint": [
         [
           0,
