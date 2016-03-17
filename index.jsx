9bcc70aacc032a4b8d913e8e30e90a82133b0531
Mikko Haapoja <me@mikkoh.com>
2016-03-16 22:06:19 -0400

    Added a function to jsx to save the file

--- index.jsx
+++ index.jsx
@@ -1,2 +1,19 @@
+/* Shims */
+require('./lib/shims/json');
+
 /* This will create the global for jsx */
 aeToJSON = require('./.');
+
+aeToJSONFile = function(prompt) {
+  prompt = prompt || '';
+
+  var f = File.saveDialog(prompt);
+
+  if(f) {
+    f.open('w');
+    
+    f.write(JSON.stringify(aeToJSON(), null, '  '));
+    
+    f.close();
+  }
+};
