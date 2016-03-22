922058288567d41e44ee4ff1a9e8098a654a958f
Mikko Haapoja <me@mikkoh.com>
2016-03-22 11:17:42 -0400

    Properties are now parsed recursively

--- src/getKeyframesForProp.js
+++ src/getKeyframesForProp.js
@@ -4,20 +4,27 @@ var getEaseForKeyFrame = require('./getEaseForKeyFrame');
 module.exports = function getKeyFramesForProp(prop) {
   var rVal = [];
 
-  // we have keyframes add all keyframes
-  if(prop.numKeys > 0) {
 
-    for(var i = 1; i <= prop.numKeys; i++) {
-      rVal.push(
-        [ prop.keyTime(i), prop.keyValue(i), getEaseForKeyFrame(prop, i) ]
-      );
+  // some properties don't have a value set on them
+  if(prop.propertyValueType !== PropertyValueType.NO_VALUE) {
+    // we have keyframes add all keyframes
+    if(prop.numKeys > 0) {
+
+      for(var i = 1; i <= prop.numKeys; i++) {
+        rVal.push(
+          [ prop.keyTime(i), prop.keyValue(i), getEaseForKeyFrame(prop, i) ]
+        );
+      }
+    // we do not have keyframes just add the first
+    } else {
+
+      // some properties don't have this
+      if(prop.valueAtTime) {
+        rVal.push(
+          [ 0, prop.valueAtTime(0, false) ]
+        );
+      }
     }
-  // we do not have keyframes just add the first
-  } else {
-
-    rVal.push(
-      [ 0, prop.valueAtTime(0, false) ]
-    );
   }
 
   return rVal;
--- src/getProperties.js
+++ src/getProperties.js
@@ -19,23 +19,79 @@ const PROP_RENAMES = {
   rotation: 'rotationZ'
 };
 
+var isFirst = true;
+
 // this function will export all properties for a layer
 module.exports = function getProperties(layer) {
-  return PROPS
-  .reduce(function(properties, property) {
-    var propOutName = PROP_RENAMES[ property ] || property;
-    var aeProperty = layer.property(property);
-    var baseValues = getNonObjectValues(aeProperty);
 
-    properties[ propOutName ] = merge(
-      baseValues,
-      {
-        keyframes: getKeyframesForProp(aeProperty),
-        propertyType: getPropertyType(baseValues.propertyType),
-        propertyValueType: getPropertyValueType(baseValues.propertyValueType)
+  // PropertyGroup
+
+  var properties = {};
+
+  var getProperties = function(layer, target) {
+    getPropertyGroupArray(layer)
+    .reduce(function(target, property) {
+      var currentTarget =  {};
+      var baseValues = getNonObjectValues(property);
+
+      // now we want to check if this property is actually a 
+      // property group
+      if(property instanceof PropertyGroup) {
+        getProperties(property, currentTarget);
       }
-    );
 
-    return properties;
-  }, {});
+      currentTarget = merge(
+        currentTarget,
+        baseValues,
+        {
+          keyframes: getKeyframesForProp(property),
+          propertyType: getPropertyType(baseValues.propertyType),
+          propertyValueType: getPropertyValueType(baseValues.propertyValueType)
+        }
+      );
+
+      // we want to remove name as it will be the objects variable name
+      // delete currentTarget.name;
+
+      target[ property.name ] = currentTarget;
+
+      return target;
+    }, target);
+  };
+
+  getProperties(layer, properties);
+
+  return properties;
+
+
+
+
+
+  // return PROPS
+  // .reduce(function(properties, property) {
+  //   var propOutName = PROP_RENAMES[ property ] || property;
+  //   var aeProperty = layer.property(property);
+  //   var baseValues = getNonObjectValues(aeProperty);
+
+  //   properties[ propOutName ] = merge(
+  //     baseValues,
+  //     {
+  //       keyframes: getKeyframesForProp(aeProperty),
+  //       propertyType: getPropertyType(baseValues.propertyType),
+  //       propertyValueType: getPropertyValueType(baseValues.propertyValueType)
+  //     }
+  //   );
+
+  //   return properties;
+  // }, {});
 };
+
+function getPropertyGroupArray(layer) {
+  var rVal = [];
+
+  for(var i = 1; i <= layer.numProperties; i++) {
+    rVal.push(layer.property(i));
+  }
+  
+  return rVal;
+}
